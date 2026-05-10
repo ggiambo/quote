4c6b874830a1db18379bbd83c63aac602a9cd9cd
Giambo <ggiambo@gmail.com>
2026-05-10 09:29:33 +0200

    Validazione ISIN, piccoli fix

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 
-require "FondInfo.php";
+require_once "FondInfo.php";
 
 $fondId = $_GET["id"];
+if ($fondId == "") {
+    http_response_code(400);
+    exit;
+}
+
+$fondRegex = "/\D{2}[a-zA-Z0-9]{9}\d/"; // https://en.wikipedia.org/wiki/International_Securities_Identification_Number#Description
+if (!preg_match($fondRegex,$fondId)) {
+    http_response_code(400);
+    exit;
+}
+
 $fondInfo = new FondInfo();
 $fondInfo->loadFundInfo($fondId);
 
